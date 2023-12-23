1db6fb246cc95a2dd498b59220837f2124724c2f
altamisatmaja <altamisfattah16@gmail.com>
2023-12-23 18:51:30 +0700

    admin: configure panel

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -9,22 +9,26 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminLoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('admin.login');
     }
 
-    public function authenticate(Request $request){
+    public function authenticate(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        if ($validator->passes()){
-            if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],
-            $request->get('remember'))){
+        if ($validator->passes()) {
+            if (Auth::guard('admin')->attempt(
+                ['email' => $request->email, 'password' => $request->password],
+                $request->get('remember')
+            )) {
                 $admin = Auth::guard('admin')->user();
 
-                if($admin->role == 2){
+                if ($admin->role == 2) {
                     return redirect()->route('admin.dashboard');
                 } else {
                     Auth::guard('admin')->logout();
@@ -39,5 +43,4 @@ class AdminLoginController extends Controller
                 ->withInput($request->only('email'));
         }
     }
-
 }
